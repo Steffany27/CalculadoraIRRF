727eeee9a342ae04f6db5d3ceeac9d0d03a459ac
Steffany27 <steffanysancosta@uni9.edu.br>
2025-11-17 23:32:35 -0300

    Update INSS.cs

--- INSS.cs
+++ INSS.cs
@@ -1,16 +1,5 @@
-public class INSS
+public class FaixaINSS
 {
-    public double Calcular(double salario)
-    {
-        if (salario <= 1412.00)
-            return salario * 0.075;
-        else if (salario <= 2666.68)
-            return salario * 0.09;
-        else if (salario <= 4000.03)
-            return salario * 0.12;
-        else if (salario <= 7786.02)
-            return salario * 0.14;
-
-        return 7786.02 * 0.14;
-    }
+    public double Ate { get; set; }
+    public double Aliquota { get; set; }
 }
